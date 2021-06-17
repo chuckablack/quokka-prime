9ce25510253893e1488b779e6bf6ac0d1f01aa09
chuck <chuck.a.black@gmail.com>
2021-06-17 13:49:56 -0700

    status graphs and adding java host monitor

--- monitors/run-monitors.sh
+++ monitors/run-monitors.sh
@@ -1,5 +1,5 @@
 quokka=${1:-localhost:5001}
-sudo python3 host_monitor.py --quokka $quokka &
+# sudo python3 host_monitor.py --quokka $quokka &
 sudo python3 host_portscan.py --quokka $quokka &
 python3 device_monitor.py --quokka $quokka &
 python3 service_monitor.py --quokka $quokka &
