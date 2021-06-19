84c0f17b1a08815a5deec97bd00825c1c1772224
chuck <chuck.a.black@gmail.com>
2021-06-19 15:23:40 -0700

    improve monitoring add discover hosts

--- monitors/java/src/quokka/home/Monitor.java
+++ monitors/java/src/quokka/home/Monitor.java
@@ -11,11 +11,63 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 public class Monitor {
+
+    private void discoverHosts() {
+
+        InetAddress localhost;
+
+        try {
+            localhost = InetAddress.getLocalHost();
+        } catch (java.net.UnknownHostException e) {
+            System.out.println("!!! discoverHosts: unable to get my local IP, error: " + e);
+            return;
+        }
+
+        byte[] ip = localhost.getAddress();
+
+        for (int i = 1; i <= 254; i++) {
+
+            InetAddress address;
+            ip[3] = (byte) i;
+            try {
+                address = InetAddress.getByAddress(ip);
+            } catch (java.net.UnknownHostException e) {
+                System.out.println("!!! discoverHosts: unable to get IP: " + Arrays.toString(ip) + ", error: " + e);
+                continue;
+            }
+
+            try {
+
+                long startTime = System.currentTimeMillis();
+                if (address.isReachable(3000)) {
+
+                    Host host = new Host();
+                    host.hostname = address.getHostName();
+                    host.ip_address = address.getHostAddress();
+
+                    host.availability = true;
+                    host.response_time = String.valueOf((float) (System.currentTimeMillis() - startTime) / 1000);
+
+                    Date date = new Date();
+                    SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss:SSS");
+                    host.last_heard = formatter.format(date);
+
+                    updateHost(host);
+                    System.out.println("discoverHosts: Discovered: " + host.hostname + " RspTime: " + host.response_time);
+
+                }
+
+            } catch (java.io.IOException e) {
+                System.out.println("!!! discoverHosts: exception pinging host: " + address.getHostAddress() + ", error: " + e);
+            }
+        }
+    }
 
     private HashMap<String, Host> getHosts() {
 
@@ -75,7 +127,7 @@ public class Monitor {
                 System.out.println("!!! pingHosts: Unable to ping host from hostname: " + host.hostname + " Error: " + e);
             }
 
-            this.updateHost(host);
+            updateHost(host);
         }
     }
 
@@ -117,10 +169,16 @@ public class Monitor {
 
     public void monitorHosts() {
 
+        int discoveryWaitMinutes = 0;
         while (true) {
 
+            if (discoveryWaitMinutes == 0) {
+                discoverHosts();
+                discoveryWaitMinutes = 60;
+            }
+
             System.out.println("\n___ Retrieving hosts from quokka-prime server _________________\n");
-            var hosts = this.getHosts();
+            var hosts = getHosts();
             for (Host host : hosts.values())
                 System.out.println("host: "+host.hostname+", ip: "+host.ip_address+", mac: "+host.mac_address);
             System.out.println();
@@ -132,6 +190,7 @@ public class Monitor {
 
             try {
                 TimeUnit.SECONDS.sleep(60);
+                discoveryWaitMinutes--;
             } catch (InterruptedException ignored) {
                 break;
             }
